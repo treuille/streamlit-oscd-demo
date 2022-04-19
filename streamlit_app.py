37adb89d8f9d78e5effcf99c89911d7113437539
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-04-19 08:47:05 -0700

    Added the name of Yulixa

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ import streamlit as st
 num_hellos = st.slider("How many hellos?", min_value=1, max_value=10)
 
 for i in range(num_hellos):
-    st.write(i, "Hello, ODSC!!")
+    st.write(i, "Hello, Yulixa!!")
